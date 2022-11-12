e570e142d81b9734fac0266454291952fff3a0c5
0xdecafdad <shaunderstine@gmail.com>
2022-11-12 00:14:24 -0500

    Update plotter_main.py
    Reflect latest functionality of driver

--- plotter_main.py
+++ plotter_main.py
@@ -1,5 +1,6 @@
 import plotter_driver
 import sys
+import time
 
 def main():
   # Get command line arguments
@@ -8,7 +9,16 @@ def main():
   #multiplier = sys.argv[3]
   #csv_file = sys.argv[4]
 
-  print(plotter_driver.convert_coords_decimal_to_degree(latitude, longitude)) 
+  #print(plotter_driver.convert_coords_decimal_to_degree(latitude, longitude))
+
+  driver = plotter_driver.create_driver("http://movable-type.co.uk/scripts/latlong.html")
+  
+  try:
+    print(plotter_driver.input_coords(driver, latitude, longitude))
+  except Exception as e:
+    print(e)
+
+  #plotter_driver.destroy_driver(driver)
 
 if __name__ == "__main__":
   main()
