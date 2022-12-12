a4785c9be8d7e4c87608d5ca862db1df19adfd97
0xdecafdad <shaunderstine@gmail.com>
2022-12-12 13:14:47 -0500

    Update plotter.py
    Remove file reading method and imports since this functionality was added to driver.py

--- plotter.py
+++ plotter.py
@@ -2,22 +2,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-import csv
-
-########################
-# FILE READING/WRITING #
-########################
-
-# Return contents of csv file as list of dictionaries
-def get_contents_csv(
-    path_to_csv):
-  with open(path_to_csv) as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    rows = []
-    for row in csv_reader:
-      rows.append(row)
-
-  return rows
 
 ############
 # SELENIUM #
